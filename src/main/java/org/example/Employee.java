dac7a59ac76905b693cd387f9e65523887f2baa7
nagarjunp <nagarjun.ha@gmail.com>
2022-12-04 00:51:20 +0530

    added required files (#1)

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -2,5 +2,14 @@ package org.example;
 
 public class Employee {
     private int id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     private String name;
 }
